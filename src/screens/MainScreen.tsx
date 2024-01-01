58f12dfff7aa02a554aad762ed1dcaba871b3dd1
ainochi-kor <kthrkdals@gmail.com>
2024-01-01 20:59:43 +0900

    @react-native-community/geolocation을 활용하여 현재 위치 구하기 및 권한 설정 추가

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {View} from 'react-native';
 import {Header} from '../components/Header/Header';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+
+// latitude 37.4994755
+// longitude 127.0352252
 
 const MainScreen: React.FC = () => {
-  // latitude 37.4994755
-  // longitude 127.0352252
+  const [currentRegion, setCurrentRegion] = useState({
+    latitude: 37.4994755,
+    longitude: 127.0352252,
+  });
+
+  const getMyLocation = useCallback(() => {
+    Geolocation.getCurrentPosition(position => {
+      setCurrentRegion({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    getMyLocation();
+  }, [getMyLocation]);
+
   return (
     <View style={{flex: 1}}>
       <Header>
@@ -14,12 +34,18 @@ const MainScreen: React.FC = () => {
       <MapView
         style={{flex: 1}}
         region={{
-          latitude: 37.4994755,
-          longitude: 127.0352252,
+          latitude: currentRegion.latitude,
+          longitude: currentRegion.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
-        }}
-      />
+        }}>
+        <Marker
+          coordinate={{
+            latitude: currentRegion.latitude,
+            longitude: currentRegion.longitude,
+          }}
+        />
+      </MapView>
     </View>
   );
 };
