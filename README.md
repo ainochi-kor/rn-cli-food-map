72dd1aaf5e00c0e205c3f3e3b912718e22e99bcb
ainochi-kor <kthrkdals@gmail.com>
2024-01-01 20:29:28 +0900

    react-native에서 ios, android 지도 추가

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-
-
 - ios Font 관련해서 중복 발생 문제는 [여기](https://eugenehwang1124.tistory.com/55) 참고하여서 해결함.
-- android Font에서 중국어가 표기되는 문제는 [여기](https://github.com/oblador/react-native-vector-icons/issues/1106) 참고하여서 해결함 
+- android Font에서 중국어가 표기되는 문제는 [여기](https://github.com/oblador/react-native-vector-icons/issues/1106) 참고하여서 해결함
+- react-native-config 이용해서 env 처리는 [여기](https://velog.io/@ddowoo/react-native-%ED%99%98%EA%B2%BD%EB%B3%80%EC%88%98-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0-.env-react-native-config) 참고함
+
+- 이 이미지가 활성화 된 상태였던거임
+![Alt text](readme/image.png)
+
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import {View} from 'react-native';
 import {Header} from '../components/Header/Header';
-import {Icon} from '../components/Icons';
+import MapView from 'react-native-maps';
 
 const MainScreen: React.FC = () => {
+  // latitude 37.4994755
+  // longitude 127.0352252
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Header>
         <Header.Title title="MAIN" />
       </Header>
-
-      <Icon name="close" size={24} color="blue" />
+      <MapView
+        style={{flex: 1}}
+        region={{
+          latitude: 37.4994755,
+          longitude: 127.0352252,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
     </View>
   );
 };
